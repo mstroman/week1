9cd411f10dd3b5992ec76e6abe838fa91da5cd6d
mstroman <52668145+mstroman@users.noreply.github.com>
2019-07-09 13:07:30 -0400

    Add files via upload

---
+++ warmUp.java
@@ -0,0 +1,20 @@
+package day2;
+
+import java.util.Scanner;
+
+public class warmUp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner kya =new Scanner(System.in);
+    System.out.println("name");
+    kya.next(); //string
+    System.out.println("age");
+    kya.nextInt(); // integer.in
+    System.out.println("favorite color");
+    kya.next(); // string 
+    System.out.println("best number");
+    kya.nextInt(); // integer.in
+	}
+}
+    
