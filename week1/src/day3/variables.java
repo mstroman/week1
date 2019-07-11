c8bf7bcad90300e1928456ee671639bc95d9e760
mstroman <52668145+mstroman@users.noreply.github.com>
2019-07-11 12:49:45 -0400

    Add files via upload

---
+++ week1/src/day3/variables.java
@@ -0,0 +1,20 @@
+package day3;
+
+public class variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	byte s=10;
+	int number=15;
+     long money=100l;
+     double basic=1.2;
+     boolean blue=true;
+    	short little=0;
+    	float pie=3.14f;
+    	char c; 0;
+    	System.out.println(money);
+    	System.out.println(" The numberis:"+ number);
+    	System.out.println(c);}
+	
+}
